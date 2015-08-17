6f9864b3030204d375dc6adddeeb3147f8f8dd16
Saloni Jain <saloni.jain@snapdeal.com>
2015-08-17 13:25:48 +0530

    UI changes

--- src/main/resources/static/js/mytour.js
+++ src/main/resources/static/js/mytour.js
@@ -10,8 +10,43 @@ var tour = new Tour({
     element: "#ProblemStatement",
     title: "ProblemStatment",
     content: "List of Problem statments"
+  },
+ 
+  {
+		element: "#services",
+		title: "Hacks",
+		content: "Current hacks that are going on",
+		placement:"left"
+  },
+  {
+	  element: "#ideaPage",
+	  title: "Ideas",
+		content: "Submit your idea and also view all current ideas",
+		placement:"left"
+  },
+  {
+	  element: "#login",
+	  title: "Idea Details",
+		content: "View the ideas and upvote the ideas that you like.",
+		placement:"left"
+  },
+  {
+	  element: "#contact",
+	  title: "Contact us",
+		content: "Lets get in touch to discuss  what we can do better",
+		placement:"top"
+  },
+  {
+		element: "#hacktube",
+		title: "HackTube",
+		content: "Catch all the latest event videos here",
+		placement:"left"
+  },
+  {
+		element: "#2048",
+		title: "2048",
+		content: "What the Fun!"
   }
-
 ]});
 
 // Initialize the tour
